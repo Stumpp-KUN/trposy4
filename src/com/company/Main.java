a53d4c5e8d555fd260ac8e88d4d4f069ed4ccf63
Stumpp <kiril.bychkov.03@mail.ru>
2022-03-25 11:57:32 +0300

    а
    а

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,9 @@ import java.util.*;
 
 public class Main {
     static Scanner scan=new Scanner(System.in);
+    private static final String articles = "FROM KFDS\n"+ "HELLO\n"+ "FDSFSDFSDFSDF\n";
     public static void main(String[] args) {
-
-        System.out.println("Введите количество статей ");
-        int j=scan.nextInt();
-        String[] articles=new String[j];
-        System.out.println("Начинайте вводить статьи ");
-        for(int i=0;i<j;i++)
-            articles[i]=scan.next();
-
-        List<String> lines = Arrays.asList(articles);
+        List<String> lines = Arrays.asList(articles.split("\n"));
         System.out.println(lines);
 
         Collections.sort(lines, new SortByLength());
